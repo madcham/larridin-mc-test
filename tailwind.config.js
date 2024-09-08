9d6da7bde37808e0273249467e0a048bf1e24284
madcham <madhu@hkmc.us>
2024-09-08 00:27:13 -0700

    commit at 12:26am Sep 8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+      './pages/**/*.{ts,tsx}',
+      './components/**/*.{ts,tsx}',
+      './app/**/*.{ts,tsx}',
+      './src/**/*.{ts,tsx}',
+      ],
+    theme: {
+      container: {
+        center: true,
+        padding: "2rem",
