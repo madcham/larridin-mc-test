9a3e5201bb61e1e1225318d774f9ab13081dd50d
madcham <madhu@hkmc.us>
2024-09-08 16:15:16 -0700

    redid larridin and index files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import React from 'react'
 import LarridinApp from '../components/LarridinApp'
 
-export default function Home() {
-  return <LarridinApp />
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-100 to-blue-100 dark:from-gray-900 dark:to-gray-800">
+      <LarridinApp />
+    </div>
+  )
 }
+
+export default Home
