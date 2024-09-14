667bae66e21ec1f24ebd3e85520c1870ac712cac
madcham <madhu@hkmc.us>
2024-09-14 01:47:58 -0700

    updated progress file

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+const ProgressPrimitive = { Root: 'div', Indicator: 'div' }
 
 import { cn } from "@/lib/utils"
 
