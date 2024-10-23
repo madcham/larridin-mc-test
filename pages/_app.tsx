606c39f722aae2e41ce3b2a8e619c68418b4bc2d
madcham <madhu@hkmc.us>
2024-10-22 21:13:13 -0700

    updated larridinApp, _apps, and tailwind.config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-} // testing comments
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
